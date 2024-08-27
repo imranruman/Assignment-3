b7923aa1a3c02ef754886e416971c91c5390115b
imranruman <imran.hossain.ruman78@gmail.com>
2024-08-27 10:51:17 +0200

    discuss next project added

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    theme: {
+      extend: {
+        backgroundImage: {
+          'hero-pattern': "url('/img/hero-pattern.svg')",
+          'footer-texture': "url('/img/footer-texture.png')",
+        }
+      }
+    }
+  }
+
+  <div class="mt-2 ml-2 mr-2 w-full h-full justify-center">
+   <img src="images/bg.jpg" alt="">
+   <div class="text-center"></div>
